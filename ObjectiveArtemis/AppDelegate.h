0bbb7d92c62a4ebf4f360aa518be266e9a1c833a
Kerberos Zhang <kerberos.zhang@gmail.com>
2013-04-19 23:31:21 +0800

    Create xcode project for develop.

---
+++ ObjectiveArtemis/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjectiveArtemis
+//
+//  Created by Kerberos Zhang on 13-4-19.
+//  Copyright (c) 2013年 Kerberos Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjectiveArtemis/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjectiveArtemis
+//
+//  Created by Kerberos Zhang on 13-4-19.
+//  Copyright (c) 2013年 Kerberos Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
